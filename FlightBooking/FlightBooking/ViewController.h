33ad7cc2f1b93e387e5179cd52df2e803069cc13
dely2p <lhyal2p@gmail.com>
2020-02-09 23:26:25 +0900

    Add function of Date Picker, toggle Switch

--- FlightBooking/FlightBooking/ViewController.h
+++ FlightBooking/FlightBooking/ViewController.h
@@ -9,7 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
-
+@property (weak, nonatomic) IBOutlet UIButton *departureDateButton;
+@property (weak, nonatomic) IBOutlet UILabel *returnDateLabel;
+@property (weak, nonatomic) IBOutlet UIButton *returnDateButton;
+@property (weak, nonatomic) IBOutlet UIDatePicker *selectDatePicker;
+- (IBAction)showReturnDateAction:(id)sender;
+- (IBAction)showDatePickerAction:(id)sender;
+- (IBAction)selectDateAction:(id)sender;
 @end
 
--- FlightBooking/FlightBooking/ViewController.m
+++ FlightBooking/FlightBooking/ViewController.m
@@ -13,11 +13,40 @@
 @end
 
 @implementation ViewController
-
+@synthesize returnDateLabel, returnDateButton, selectDatePicker, departureDateButton;
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    returnDateLabel.hidden = YES;
+    returnDateButton.hidden = YES;
+    selectDatePicker.hidden = YES;
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing: YES];
+    selectDatePicker.hidden = YES;
+}
 
+- (IBAction)showReturnDateAction:(id)sender {
+    if(returnDateButton.hidden == YES) {
+        returnDateLabel.hidden = NO;
+        returnDateButton.hidden = NO;
+    }else {
+        returnDateLabel.hidden = YES;
+        returnDateButton.hidden = YES;
+    }
+}
+
+- (IBAction)showDatePickerAction:(id)sender {
+    selectDatePicker.hidden = NO;
+}
+
+- (IBAction)selectDateAction:(id)sender {
+    NSLog(@"description : %@", [sender description]);
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    formatter.dateFormat = @"YY-MM-dd hh:mma";
+    
+    NSString *dateString = [formatter stringFromDate: [selectDatePicker date]];
+    NSLog(@"%@", dateString);
+    [departureDateButton setTitle: dateString forState: UIControlStateNormal];
+}
 @end

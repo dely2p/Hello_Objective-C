5e588d96736be63d43db413e75e8ece7d0eb9e16
dely2p <lhyal2p@gmail.com>
2020-02-09 18:24:28 +0900

    Create FlightBooking project

---
+++ FlightBooking/FlightBooking/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FlightBooking
+//
+//  Created by dely on 09/02/2020.
+//  Copyright © 2020 dely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ FlightBooking/FlightBooking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlightBooking
+//
+//  Created by dely on 09/02/2020.
+//  Copyright © 2020 dely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
